3894034b48127466175fafc2493ff2c8575cb0ae
daminitomer <contact.daminichaudhary@gmail.com>
2021-12-09 22:47:27 +0530

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    state{
+        count=0;
+    }
+    render() {
+        return (
+        <div>
+            <h1 id="marco-polo">marco</h1>
+            <button id="marco-polo-toggler" onClick="swap">polo</button>
+        </div>
+        );
+    }
+    function swap(){
+        let x =count+1,
+        if(count%2==0)
+        {
+            button.h1="marco";
+        }
+}
+
+export default Counter;
